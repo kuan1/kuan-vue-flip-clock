e59143df4441d06a3a0656982f539a988829a099
luzhongkuan <luzhongk@126.com>
2019-07-04 22:50:45 +0800

    fix

--- src/FlipItem.vue
+++ src/FlipItem.vue
@@ -1,122 +1,14 @@
 <template>
   <ul class="flip">
-    <li>
+    <li v-for="item in 10" :key="item">
       <a href="#">
         <div class="up">
           <div class="shadow"></div>
-          <div class="inn">0</div>
+          <div class="inn">{{item - 1}}</div>
         </div>
         <div class="down">
           <div class="shadow"></div>
-          <div class="inn">0</div>
-        </div>
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <div class="up">
-          <div class="shadow"></div>
-          <div class="inn">1</div>
-        </div>
-        <div class="down">
-          <div class="shadow"></div>
-          <div class="inn">1</div>
-        </div>
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <div class="up">
-          <div class="shadow"></div>
-          <div class="inn">2</div>
-        </div>
-        <div class="down">
-          <div class="shadow"></div>
-          <div class="inn">2</div>
-        </div>
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <div class="up">
-          <div class="shadow"></div>
-          <div class="inn">3</div>
-        </div>
-        <div class="down">
-          <div class="shadow"></div>
-          <div class="inn">3</div>
-        </div>
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <div class="up">
-          <div class="shadow"></div>
-          <div class="inn">4</div>
-        </div>
-        <div class="down">
-          <div class="shadow"></div>
-          <div class="inn">4</div>
-        </div>
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <div class="up">
-          <div class="shadow"></div>
-          <div class="inn">5</div>
-        </div>
-        <div class="down">
-          <div class="shadow"></div>
-          <div class="inn">5</div>
-        </div>
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <div class="up">
-          <div class="shadow"></div>
-          <div class="inn">6</div>
-        </div>
-        <div class="down">
-          <div class="shadow"></div>
-          <div class="inn">6</div>
-        </div>
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <div class="up">
-          <div class="shadow"></div>
-          <div class="inn">7</div>
-        </div>
-        <div class="down">
-          <div class="shadow"></div>
-          <div class="inn">7</div>
-        </div>
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <div class="up">
-          <div class="shadow"></div>
-          <div class="inn">8</div>
-        </div>
-        <div class="down">
-          <div class="shadow"></div>
-          <div class="inn">8</div>
-        </div>
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <div class="up">
-          <div class="shadow"></div>
-          <div class="inn">9</div>
-        </div>
-        <div class="down">
-          <div class="shadow"></div>
-          <div class="inn">9</div>
+          <div class="inn">{{item - 1}}</div>
         </div>
       </a>
     </li>
